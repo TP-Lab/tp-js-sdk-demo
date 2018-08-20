7dca311a7f8f9a4f8920bc6dfab7ed8e13a1a7f1
chendatony31 <31647753@qq.com>
2018-08-20 15:37:46 +0800

    add enu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,6 +25,7 @@
         <button @click="getWalletList('eth')">getWalletList(eth)</button>
         <button @click="getWalletList('moac')">getWalletList(moac)</button>
         <button @click="getWalletList('eos')">getWalletList(eos)</button>
+        <button @click="getWalletList('enu')">getWalletList(enu)</button>
         <div class="getWalletListLog">
 
         </div>
@@ -58,6 +59,34 @@
       </div>
     </div>
     <div class="item">
+      <h3>1.6 tp.getCurrentWallet</h3>
+      <div class="demo-content">
+        <button @click="getCurrentWallet">getCurrentWallet</button>
+        <div class="getCurrentWalletLog">
+
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>1.7 tp.getWallets</h3>
+      <div class="demo-content">
+        <button @click="getWallets">getWallets</button>
+        <div class="getWalletsLog">
+
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>1.8 tp.sign</h3>
+      <div class="demo-content">
+        <textarea style="width:100%" id="push-actions" col="30" rows="2" v-model="appidParams"></textarea><br>
+        <button @click="sign">sign</button>
+        <div class="signLog">
+
+        </div>
+      </div>
+    </div>
+    <div class="item">
       <h3>2.1 tp.eosTokenTransfer</h3>
       <div class="demo-content">
         <p>from：<input v-model="fromEos" type="text"> </p>
@@ -67,6 +96,7 @@
         <p>Contract: <input v-model="contractEos" type="text"></p>
         <p>precision: <input type="text" v-model="precisionEos"> </p>
         <p>memo: <input  v-model="memoEos" type="text"></p>
+        <p>address: <input v-model="addressEos" type="text"> </p>
         <button @click="eosTokenTransfer">eosTokenTransfer</button>
         <div class="eosTokenTransferLog">
 
@@ -87,7 +117,7 @@
     <div class="item">
       <h3>2.3 tp.getTableRows</h3>
       <div class="demo-content">
-        <textarea style="width:100%" id="push-actions" col="30" rows="5" v-model="tableRows"></textarea><br>
+        <textarea style="width:100%" id="push-actions" col="30" rows="5" v-model="eosTableRows"></textarea><br>
         <button @click="getTableRows">getTableRows</button>
         <div class="getTableRowsLog">
         </div>
@@ -103,17 +133,17 @@
         </div>
       </div>
     </div>
-    <!-- <div class="item">
-      <h3>2.5 tp.getEosTransactionRecord (ios 0.37+ , android 0.42+)</h3>
+    <div class="item">
+      <h3>2.5 tp.getEosTransactionRecord</h3>
       <div class="demo-content">
-        <textarea style="width:100%" id="push-actions" col="30" rows="5" v-model="transactionParams"></textarea><br>
+        <textarea style="width:100%" id="push-actions" col="30" rows="5" v-model="eosTransactionParams"></textarea><br>
         <button @click="getEosTransactionRecord">getEosTransactionRecord</button>
         <div class="getEosTransactionRecordLog">
         </div>
       </div>
-    </div> -->
+    </div>
     <div class="item">
-      <h3>2.6 tp.getEosAccountInfo (ios 0.37+ , android 0.42+)</h3>
+      <h3>2.6 tp.getEosAccountInfo</h3>
       <div class="demo-content">
         <button @click="getEosAccountInfo">getEosAccountInfo</button>
         <div class="getEosAccountInfoLog">
@@ -156,6 +186,74 @@
         </div>
       </div>
     </div>
+
+    <div class="item">
+      <h3>4.1 tp.enuTokenTransfer</h3>
+      <div class="demo-content">
+        <p>from：<input v-model="fromEnu" type="text"> </p>
+        <p>to: <input v-model="toEnu"  type="text" ></p> 
+        <p>amount: <input v-model="amountEnu"  type="text" ></p> 
+        <p>Token: <input v-model="tokenNameEnu" type="text"></p>
+        <p>Contract: <input v-model="contractEnu" type="text"></p>
+        <p>precision: <input type="text" v-model="precisionEnu"> </p>
+        <p>memo: <input  v-model="memoEnu" type="text"></p>
+        <p>address: <input v-model="addressEnu" type="text"> </p>
+        <button @click="enuTokenTransfer">enuTokenTransfer</button>
+        <div class="enuTokenTransferLog">
+
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>4.2 tp.pushEnuAction</h3>
+      <div class="demo-content">
+        Change the 'from' and 'to' fields into your custom account at least.
+        <textarea style="width:100%" id="push-actions" col="30" rows="10" v-model="actionsEnu"></textarea><br>
+        <button @click="pushEnuAction">pushEnuAction</button>
+        <div class="pushEnuActionLog">
+
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>4.3 tp.getEnuTableRows</h3>
+      <div class="demo-content">
+        <textarea style="width:100%" id="push-actions" col="30" rows="5" v-model="enuTableRows"></textarea><br>
+        <button @click="getEnuTableRows">getEnuTableRows</button>
+        <div class="getEnuTableRowsLog">
+        </div>
+      </div>
+    </div>
+    
+    <div class="item">
+      <h3>4.4 tp.getEnuBalance</h3>
+      <div class="demo-content">
+        <button @click="getEnuBalance">getEnuBalance</button>
+        <div class="getEnuBalanceLog">
+
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>4.5 tp.getEnuTransactionRecord</h3>
+      <div class="demo-content">
+        <textarea style="width:100%" id="push-actions" col="30" rows="5" v-model="enuTransactionParams"></textarea><br>
+        <button @click="getEnuTransactionRecord">getEnuTransactionRecord</button>
+        <div class="getEnuTransactionRecordLog">
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>4.6 tp.getEnuAccountInfo</h3>
+      <div class="demo-content">
+        <button @click="getEnuAccountInfo">getEnuAccountInfo</button>
+        <div class="getEnuAccountInfoLog">
+
+        </div>
+      </div>
+    </div>
+
+    
   </div>
 </template>
 
@@ -174,6 +272,13 @@ export default {
       tokenNameEos: '',
       precisionEos: '',
       memoEos: '',
+      fromEnu: '',
+      toEnu: '',
+      amountEnu: '',
+      contractEnu: '',
+      tokenNameEnu: '',
+      precisionEnu: '',
+      memoEnu: '',
       fromMoac: '',
       toMoac: '',
       amountMoac: '',
@@ -181,9 +286,13 @@ export default {
       tokenNameMoac: '',
       decimalMoac: '',
       gasLimitMoac: '',
-      transactionParams: '{"page": 2, "account": "itokenpocket", "count": 20, "token": "EOS", "sort": "desc", "contract": "eosio.token"}',
-      tableRows: '{"json": true, "code": "theeosbutton", "scope": "theeosbutton","table": "accstates","lower_bound": "10", "limit": 30}',
-      actionsEos: '{"actions":[{"account":"eosio.token","name":"transfer","authorization":[{"actor":"itokenpocket","permission":"active"}],"data":{"from":"itokenpocket","to":"itokenpocket","quantity":"0.0001 EOS","memo":"test sdk"}},{"account":"eosio","name":"delegatebw","authorization":[{"actor":"itokenpocket","permission":"active"}],"data":{"from":"itokenpocket","receiver":"itokenpocket","stake_net_quantity":"0.0100 EOS","stake_cpu_quantity":"0.0100 EOS","transfer":0}}]}'
+      appidParams: '{"appid": "s9QY7ZYad6Xm"}',
+      eosTransactionParams: '{"start": 2, "account": "itokenpocket", "count": 20, "token": "EOS", "sort": "desc", "contract": "eosio.token"}',
+      eosTableRows: '{"json": true, "code": "theeosbutton", "scope": "theeosbutton","table": "accstates","lower_bound": "10", "limit": 30}',
+      actionsEos: '{"actions":[{"account":"eosio.token","name":"transfer","authorization":[{"actor":"itokenpocket","permission":"active"}],"data":{"from":"itokenpocket","to":"itokenpocket","quantity":"0.0001 EOS","memo":"test sdk"}},{"account":"eosio","name":"delegatebw","authorization":[{"actor":"itokenpocket","permission":"active"}],"data":{"from":"itokenpocket","receiver":"itokenpocket","stake_net_quantity":"0.0100 EOS","stake_cpu_quantity":"0.0100 EOS","transfer":0}}],"account": "itokenpocket", "address": "EOS7ds9A9FGDsKrdymQ4ynKbMgbCVUdBsosEpsLTqvg6icyBMcQUm"}',
+      enuTransactionParams: '{"start": 2, "account": "accommodated", "count": 20, "token": "ENU", "sort": "desc", "contract": "enu.token"}',
+      enuTableRows: '{"json": true, "code": "pickpickteam", "scope": "pickpickteam","table": "question","lower_bound": "5", "limit": 8}',
+      actionsEnu: '{"actions":[{"account":"enu.token","name":"transfer","authorization":[{"actor":"accommodated","permission":"active"}],"data":{"from":"accommodated","to":"accommodated","quantity":"0.0001 ENU","memo":"test sdk"}},{"account":"enumivo","name":"delegatebw","authorization":[{"actor":"accommodated","permission":"active"}],"data":{"from":"accommodated","receiver":"accommodated","stake_net_quantity":"0.0100 ENU","stake_cpu_quantity":"0.0100 ENU","transfer":0}}], "account": "accommodated", "address": "ENU58Y6KWWVTa3nGVgPmAWyxAXJoyy7kkKjbStPW5C63QJdYJ1TtP"}'
     }
   },
   created() {
@@ -210,6 +319,11 @@ export default {
         $('.getDeviceIdLog').append(JSON.stringify(data));
       })
     },
+    getWallets() {
+      tp.getWallets().then(data => {
+        $('.getWalletsLog').append(JSON.stringify(data));
+      })
+    },
     shareNewsToSNS() {
       tp.shareNewsToSNS({
         title: 'TokenPocket',
@@ -225,6 +339,18 @@ export default {
         $('.invokeQRScannerLog').append(JSON.stringify(data));
       })
     },
+    getCurrentWallet() {
+      tp.getCurrentWallet().then(data => {
+        $('.getCurrentWalletLog').append(JSON.stringify(data));
+      })
+    },
+    sign() {
+      var params = JSON.parse(this.appidParams);
+      $('.signLog').append(JSON.stringify(params));
+      tp.sign(params).then(data => {
+        $('.signLog').append(JSON.stringify(data));
+      })
+    },
     eosTokenTransfer() {
       var params = {
         from: this.fromEos,
@@ -233,7 +359,8 @@ export default {
         contract: this.contractEos,
         precision: this.precisionEos,
         memo: this.memoEos,
-        tokenName: this.tokenNameEos
+        tokenName: this.tokenNameEos,
+        address: this.addressEos,
       }
       $('.eosTokenTransferLog').append(JSON.stringify(params));
       tp.eosTokenTransfer(params).then(data => {
@@ -255,7 +382,7 @@ export default {
     },
     getTableRows() {
       try {
-        var params = JSON.parse(this.tableRows);
+        var params = JSON.parse(this.eosTableRows);
 
         tp.getTableRows(params).then(data => {
           $('.getTableRowsLog').append(JSON.stringify(data));
@@ -267,7 +394,7 @@ export default {
     },
     getEosTransactionRecord() {
       try {
-        var params = JSON.parse(this.transactionParams);
+        var params = JSON.parse(this.eosTransactionParams);
 
         tp.getEosTransactionRecord(params).then(data => {
           $('.getEosTransactionRecordLog').append(JSON.stringify(data));
@@ -297,6 +424,83 @@ export default {
         $('.getEosAccountInfoLog').append(JSON.stringify(data));
       })
     },
+
+    // enu
+    enuTokenTransfer() {
+      var params = {
+        from: this.fromEnu,
+        to: this.toEnu,
+        amount: this.amountEnu,
+        contract: this.contractEnu,
+        precision: this.precisionEnu,
+        memo: this.memoEnu,
+        tokenName: this.tokenNameEnu,
+        address: this.addressEnu
+      }
+      $('.enuTokenTransferLog').append(JSON.stringify(params));
+      tp.enuTokenTransfer(params).then(data => {
+        $('.enuTokenTransferLog').append(JSON.stringify(data));
+      })
+    },
+    pushEnuAction() {
+      try {
+        var params = JSON.parse(this.actionsEnu);
+
+        tp.pushEnuAction(params).then(data => {
+          $('.pushEnuActionLog').append(JSON.stringify(data));
+        })
+      }
+      catch(e) {
+        console.log(e);
+      }
+      
+    },
+    getEnuTableRows() {
+      try {
+        var params = JSON.parse(this.enuTableRows);
+
+        tp.getEnuTableRows(params).then(data => {
+          $('.getEnuTableRowsLog').append(JSON.stringify(data));
+        });
+      }
+      catch(e) {
+        console.log(e);
+      }
+    },
+    getEnuTransactionRecord() {
+      try {
+        var params = JSON.parse(this.enuTransactionParams);
+
+        tp.getEnuTransactionRecord(params).then(data => {
+          $('.getEnuTransactionRecordLog').append(JSON.stringify(data));
+        });
+      }
+      catch(e) {
+        console.log(e);
+      }
+    },
+    getEnuBalance() {
+      var params = {
+        account: 'accommodated',
+        contract: 'ltsonenumivo', 
+        symbol: 'LTS'
+      }
+      $('.getEnuBalanceLog').append(JSON.stringify(params));
+      tp.getEnuBalance(params).then(data => {
+        $('.getEnuBalanceLog').append(JSON.stringify(data));
+      })
+    },
+    getEnuAccountInfo() {
+      var params = {
+        account: 'accommodated'
+      }
+      $('.getEnuAccountInfoLog').append(JSON.stringify(params));
+      tp.getEnuAccountInfo(params).then(data => {
+        $('.getEnuAccountInfoLog').append(JSON.stringify(data));
+      })
+    },
+
+    // moac eth
     moacTokenTransfer() {
       var params = {
         from: this.fromMoac,
